8a22d73c3d23e2ffd5b0f499d0ad436d34d10d1b
Robbie Leslie <robbie.w.leslie@gmail.com>
2025-11-13 08:22:16 -0500

    Final project skeleton

---
+++ Final_Project.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/i2c.h"
+#include "hardware/dma.h"
+#include "hardware/pio.h"
+#include "hardware/timer.h"
+#include "hardware/clocks.h"
+#include "hardware/uart.h"
+
+#include "TFTMaster.h"
+
+
+
+
+int main()
+{
+
+}
